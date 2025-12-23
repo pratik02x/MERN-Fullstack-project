822dec7b6030afbb744e29607a8b7da0cf9493ed
Pratik <pratikpatil12324@gmail.com>
2025-12-24 02:59:38 +0530

    student api

---
+++ BACKEND/Server/server.js
@@ -0,0 +1,22 @@
+const express=require("express");
+
+
+const app=express();
+
+const {authuser,authorization}=require("./utils/auth");
+// const commenApi=require("./routes/common_Api");
+// const adminRouter=require("./routes/Admin");
+const studentRouter=require("./routes/student");
+
+
+app.use(express.json())
+app.use(authuser);
+// app.use(authorization);
+// app.use("/common",commenApi);
+// app.use("/admin",adminRouter);
+app.use("/student",studentRouter);
+
+app.listen(4000,"localhost",()=>{
+    console.log("server start at port 4000");
+})
+
