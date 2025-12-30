ca17454de6ef0f838d21bbef6ab3bdca1024fda3
Pratik Patil <pratikpatil12324@gmail.com>
2025-12-30 17:15:25 +0530

    Delete BACKEND/Server directory

--- BACKEND/Server/db/pool.js
+++
@@ -1,9 +0,0 @@
-const mysql2=require("mysql2");
-const pool=mysql2.createPool({
-    host:"localhost",
-    user:"root",
-    password:"mysql1234",
-    database:"project_db"
-})
-
-module.exports=pool;
--- BACKEND/Server/utils/config.js
+++
@@ -1,3 +0,0 @@
-const SECRET="rfgerkejbfvkiebfekifbeoiekebcic";
-
-module.exports={SECRET};
