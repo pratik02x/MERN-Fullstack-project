f44c3eed71b472e36cf6419fc508871ef54b013f
gm983468@gmail.com <gm983468@gmail.com>
2025-12-30 18:19:46 +0530

    GetAllcourese

--- F/Frontend/app1/src/pages/Admin/GetAllCourses.jsx
+++ F/Frontend/app1/src/pages/Admin/GetAllCourses.jsx
@@ -21,12 +21,7 @@ function GetAllCourses() {
     }
   };
 
-  const formatDate = (dateStr) => {
-    if (!dateStr) return null;
-    return new Date(dateStr).toLocaleDateString('en-GB', {
-      day: '2-digit', month: 'short', year: 'numeric'
-    });
-  };
+  
 
   return (
     <div className="container mt-4">
