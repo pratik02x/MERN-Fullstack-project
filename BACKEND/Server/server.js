dbddbc5e8b3e704273f5a2c83c2303fa7b7ba4b1
Pratik Patil <pratikpatil12324@gmail.com>
2026-01-20 23:06:05 +0530

    Fix formatting of express app initialization

--- BACKEND/Server/server.js
+++ BACKEND/Server/server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const cors = require("cors");
 const path = require('path');
 
-const app=express();
+const app=express()
 
 const {authuser,authorization}=require("./utils/auth");
 const commenApi=require("./routes/common_Api");
